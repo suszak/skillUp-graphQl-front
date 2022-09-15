e08e339370533654a29f83d29fb84fbfc356e70b
Mateusz Greń <matys.1996@wp.pl>
2022-09-15 11:19:04 +0200

    init: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>GraphQl SkillUp</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+const path = require('path');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve:{
+    alias:{
+      '@' : path.resolve(__dirname, './src')
+    },
+  },
+  plugins: [react()],
+});
